0eb13c945d31fba57934c1e18bf7a48a56d597b1
Talal Alzayed <79800154+talalzd@users.noreply.github.com>
2026-03-21 00:26:09 +0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import TalalSite from './App.jsx'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <TalalSite />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
